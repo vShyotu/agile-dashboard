e084cabfdee14b871143214bda122fdcd61209c7
Grant Hardman <grant.r.hardman@outlook.com>
2021-12-20 20:13:40 +0000

    Add initial pages and routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-const App = () => 
-    <div className="App">
-      <h1>My App</h1>
-    </div>
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
+import Home from "./pages/Home";
+import Backlog from "./pages/Backlog";
+import Board from "./pages/Board";
+
+const App = () => {
+  return (
+    <>
+      <h1>My App</h1>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/backlog">Backlog</Link>
+              </li>
+              <li>
+                <Link to="/board">Board</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <Routes>
+          <Route path="/backlog" element={<Backlog />} />
+          <Route path="/board" element={<Board />} />
+          <Route index element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
 export default App;
---
+++ src/pages/Backlog/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Backlog = () => {
+  return <div>Backlog</div>;
+};
+
+export default Backlog;
